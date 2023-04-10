d82de4216ae38f7d27ff5b640941059a1f9ac5cb
Juan Ignacio Tejada <jtejada@lubee.com.ar>
2023-04-10 20:50:48 -0300

    Se agrego logica de llamada a la api, cambios en el favicon, cambios en los diferentes componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
-import ImgCypto from "./img/imagencriptos.png";
-import Form from "./components/Form";
+import Formulario from "./components/Formulario";
+import Resultado from "./components/Resultado";
+import Spinner from "./components/Spinner";
+import ImagenCripto from "./img/imagen-criptos.png";
 
-const Heading = styled.h1`
-  color: red;
-  text-align: center;
-  margin-top: 2rem;
-`;
 const Contenedor = styled.div`
   max-width: 900px;
-  width: 90%;
   margin: 0 auto;
+  width: 90%;
   @media (min-width: 992px) {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
@@ -24,21 +21,63 @@ const Imagen = styled.img`
   margin: 100px auto 0 auto;
   display: block;
 `;
-const Header = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
   text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `;
+
 function App() {
+  const [monedas, setMonedas] = useState({});
+  const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(monedas).length > 0) {
+      const cotizarCripto = async () => {
+        setCargando(true);
+        setResultado({});
+
+        const { moneda, criptomoneda } = monedas;
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        setResultado(resultado.DISPLAY[criptomoneda][moneda]);
+
+        setCargando(false);
+      };
+
+      cotizarCripto();
+    }
+  }, [monedas]);
+
   return (
     <Contenedor>
-      <Imagen src={ImgCypto} alt="Imagen Criptomonedas" />
-      <Header>
-        <Heading>Cotiza tus Criptos</Heading>
-        <Form />
-      </Header>
+      <Imagen src={ImagenCripto} alt="imagenes criptomonedas" />
+
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario setMonedas={setMonedas} />
+
+        {cargando && <Spinner />}
+        {resultado.PRICE && <Resultado resultado={resultado} />}
+      </div>
     </Contenedor>
   );
 }
